21f83a81071d5ea6f56e50fa41cf094e90b00db1
KostyaVid <constat@live.com>
2023-01-27 21:07:07 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Импортируем основные настройки
+require('settings')
+
+-- Выключаем встроенные плагины
+require('disable_builtins')
+
+-- Подключаем плагины
+require('plugins/install_list/init')
+
+-- Подгружаем горячие клавиши
+require('keybindings/main')
